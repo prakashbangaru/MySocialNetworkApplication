cb3cffb91ba4beb0618b9e3cca78d47c3435c47d
prakashbangaru <bangaruvprakash@gmail.com>
2018-04-04 22:24:35 -0400

    MySocialNetworkApplication_FirstCommit

---
+++ MySocialNetworkApp/src/main/java/com/example/demo/MySocialNetworkAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySocialNetworkAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySocialNetworkAppApplication.class, args);
+	}
+}
---
+++ MySocialNetworkApp/src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@Controller
+public class UserController {
+	@GetMapping(value="/")
+	public ModelAndView renderPage() {
+		ModelAndView indexPage = new ModelAndView();
+		indexPage.setViewName("index");
+		return indexPage;
+		
+	}
+
+
+}
